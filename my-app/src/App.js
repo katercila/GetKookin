d4e789a323ab2b199506536f8b0ce2f123c9c00d
Katerin <kercila9@gmail.com>
2024-11-08 21:02:00 -0500

    trying tailwind nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
+import React from "react";
 import './App.css';
-import MeasurementConverter from './Components/Calc/Calculator';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MeasurementConverter from "./Components/Calc/Calculator";
+import StickyNavbar from "./Components/NavBar/Nav";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <MeasurementConverter />
-      </header>
-    </div>
+    <Router>
+      <StickyNavbar />
+      <Routes>
+        <Route exact path="/" element={<MeasurementConverter />} />      
+      </Routes>
+    </Router> 
+    
   );
 }
 
